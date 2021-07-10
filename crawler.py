c2bcac1ee9dec0b01143bc1df56923f5ed19b39e
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-07-10 15:19:26 +0530

    added minimum search threshold, blacklist, and phobos

--- crawler.py
+++ crawler.py
@@ -8,6 +8,7 @@ import threading
 import pandas as pd
 from requests import get
 from time import sleep
+from base64 import b64decode
 from retriever.searcher import search
 
 def remove_html_tags(text):
@@ -40,21 +41,21 @@ def main():
         data,          \
         children,      \
         locked,        \
-        depth_counter, \
+        depth,         \
         should_exit,   \
         pos_q,         \
-        neg_q
+        neg_q,         \
+        blacklist
 
     cols = lambda: shutil.get_terminal_size().columns
     clrline = lambda : print('\r' + ' '*cols() + '\r', end="", flush=True)
     q_len = lambda: len(pos_q) + len(neg_q)
-    print_status = lambda: print((f'\r{" "*cols()}\r{green}Depth: {depth_counter}  Queued: {q_len()}  '
+    print_status = lambda: print((f'\r{" "*cols()}\r{green}Depth: {depth}  Queued: {q_len()}  '
         f'Searched: {len(data)}  Current: {curr[:70]+"..." if len(curr)>75 else curr}{reset}'), end='', flush=True)
 
-    while True:
-
-        if should_exit:
-            exit(0)
+    while depth < 5:
+        # if should_exit:
+        #     exit(0)
 
         while q_len() == 0 or locked:
             sleep(0.1)
@@ -64,7 +65,7 @@ def main():
             timeout = 10
         else:
             curr = neg_q.pop(0)
-            timeout = 8
+            timeout = 7
 
         print_status()
 
@@ -72,7 +73,8 @@ def main():
 
         # if subdirs of site have been visited more than 20 times then dont visit site
         # again
-        if len(list(filter(lambda x: x.startswith(curr[:delim]), visited))) < 20:
+        # if len(list(filter(lambda x: x.startswith(curr[:delim]), visited))) < 50:
+        if curr not in visited:
             visited.append(curr)
         else:
             continue
@@ -85,12 +87,6 @@ def main():
                 },
                 timeout=timeout).text
 
-            if len(tosearch) > 30:
-                # Doing this because after ranking pages we can grab url easily
-                data[curr] = curr + ' || ' + remove_html_tags(tosearch)
-            else:
-                continue
-
         except Exception as err:
             print(f'\r{" "*cols()}\r{red}{curr} took too long to respond{reset}')
 
@@ -100,54 +96,64 @@ def main():
 
             continue
 
-        complete_urls = set(re.findall(url_regex, tosearch))
-        unique_complete = complete_urls - set(visited) - (set(pos_q).union(set(neg_q)))
+        if len(tosearch) < 30 or any(list(map(lambda x: x in tosearch, blacklist))):
+            continue
+        else:
+            # Doing this because after ranking pages we can grab url easily
+            data[curr] = curr + ' || ' + remove_html_tags(tosearch)
 
         score, _ = rank_data(' '.join(query), {'1': tosearch})
-        # print(f"score for {curr}:", score)
+        # print(curr, score)
 
-        if score[0] > 0.07:
+        if score[0] > 0.02:
+            complete_urls = set(re.findall(url_regex, tosearch))
+            unique_complete = complete_urls - set(visited) - (set(pos_q).union(set(neg_q)))
+
+            # print(f"score for {curr}:", score)
+
+            # if score[0] > 0.1:
             subdirs = set(map(lambda x: curr[:delim] + x if x.startswith('/') else curr[:delim] + '/' + x, 
                 re.findall(subdir_regex, tosearch)))
             unique_subdirs = subdirs - set(visited) - (set(pos_q).union(set(neg_q)))
 
             unique = unique_complete.union(unique_subdirs)
-        else:
-            unique = unique_complete
+            # else:
+            #     unique = unique_complete
 
-        positive_set = set(filter(lambda x: any(list(keyword in x.lower() for keyword in query)), unique))
-        negative_set = unique - positive_set
+            # Positive set contains all the url that contain one or more keywords in them
+            positive_set = set(filter(lambda x: any(list(keyword in x.lower() for keyword in query)), unique))
+            # Everything else goes in negative_set
+            negative_set = unique - positive_set
 
-        pos_q.extend(list(positive_set))
-        neg_q.extend(list(negative_set)[:10])
+            pos_q.extend(list(positive_set))
+            neg_q.extend(list(negative_set))
 
+            if len(unique) > 0:
+                if len(negative_set) > 0:
+                    clrline()
+                    print('\n'.join(list(negative_set)[:30]))
+                if len(positive_set) > 0:
+                    print('\n'.join(positive_set))
 
-        if len(unique) > 0:
-            if len(negative_set) > 0:
-                clrline()
-                print('\n'.join(list(negative_set)[:10]))
-            if len(positive_set) > 0:
-                print('\n'.join(positive_set))
-                
-            children[curr] = [list(positive_set), list(negative_set)]
+                children[curr] = [list(positive_set), list(negative_set)]
 
-        else:
-            print(f'\r{" "*cols()}\r{red}No links found in {curr}{reset}')
+            else:
+                print(f'\r{" "*cols()}\r{red}No links found in {curr}{reset}')
 
-        if not locked and len(data) > 60:
+        if not locked and len(data) > 20*(depth+1):
             locked = True
 
             date_copy = data.copy()
             scores, sorted_pages = rank_data(' '.join(query), date_copy)
 
-            if depth_counter == 1:
-                prevscores = scores
-            else:
-                if scores[0] < prevscores[0]:
-                    clrline()
-                    print(f'{red}Better results in previous depth, exiting{reset}')
-                    should_exit = True
-                    exit(0)
+            # if depth == 1:
+            #     prevscores = scores
+            # else:
+            #     if sum(scores[0]) < sum(prevscores[:5]):
+            #         clrline()
+            #         print(f'{red}Better results in previous depth, exiting{reset}')
+            #         should_exit = True
+            #         exit(0)
 
             data = {}
 
@@ -163,14 +169,14 @@ def main():
             toprint = separator.join(sorted_urls[:5])
 
             clrline()
-            depth_counter += 1
-            print(f'{yellow}\nBest result at depth {depth_counter}: {toprint}{reset}\n')
+            depth += 1
+            print(f'{yellow}\nBest result at depth {depth}: {toprint}{reset}\n')
 
             with open('results.txt', 'a') as f:
                 a = sorted_pages[0].index(' || ')
                 b = sorted_pages[1].index(' || ')
-                f.write((f'{depth_counter}. {sorted_urls[0]} - {sorted_pages[0][a+6:a+56]}\n'
-                        f'{depth_counter}. {sorted_urls[1]} - {sorted_pages[1][b+6:b+56]}\n\n'))
+                f.write((f'{depth}. {sorted_urls[0]} - {sorted_pages[0][a+4:a+56]}\n'
+                        f'{depth}. {sorted_urls[1]} - {sorted_pages[1][b+4:b+56]}\n\n'))
 
             for url in reversed(sorted_urls[:3]):
                 # If url has children shift all children to beginning of q
@@ -185,15 +191,16 @@ def main():
 if __name__ == '__main__':
     query = list(map(lambda x: x.lower(), [
             'flipkart',
-            'data leak',
+            'data',
             'leaked',
             'cybercrime',
             'hacking',
         ]))
 
     # query = list(map(lambda x: x.lower(), [
-    #         "dominos",
-    #         'leak',
+    #         "cats",
+    #         "dogs",
+    #         "animals"
     #     ]))
 
 
@@ -224,7 +231,7 @@ if __name__ == '__main__':
     data = {}
     prevscores = [0]
 
-    depth_counter = 0
+    depth = 0
     locked = False
     should_exit = False
 
@@ -250,12 +257,35 @@ if __name__ == '__main__':
     # Clear results file
     open('results.txt', 'w').close()
 
+    blacklist = []
+    with open('blacklist.txt') as f:
+        for line in f:
+            blacklist.append(b64decode(line).decode())
+
+    print(f'{yellow}Grabbing results from phobos{reset}')
+    neg_q = []
+    for i in range (1,4):
+        url = f"http://phobosxilamwcg75xt22id7aywkzol6q6rfl2flipcqoc4e4ahima5id.onion/search?query={'+'.join(query)}&p={i}"
+        headers = {
+                'Referer': url
+            }
+        phobos_page = get(url, 
+            proxies={
+                'http':'socks5h://localhost:9050',
+                'https':'socks5h://localhost:9050'
+            },
+            headers = headers,
+            timeout=10).text
+        neg_q.extend(re.findall('<a class="titles" href="(.*?)"', phobos_page))
+
+    print(f'{yellow}Grabbing results from ahmia{reset}')
     url = f"https://ahmia.fi/search/?q={'+'.join(query)}"
-    engine_page = get(url).text
-    neg_q = re.findall(r'redirect_url=(.*?)">', engine_page)
+    ahmia_page = get(url).text
+    neg_q.extend(re.findall(r'redirect_url=(.*?)">', ahmia_page))
+
     pos_q = []
 
     threads=[]
-    for _ in range(6):
+    for _ in range(7):
         threads.append(threading.Thread(target=main))
         threads[-1].start()
