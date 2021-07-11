1d67ecba12ce3bccc53307bc664142dc90191b90
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-07-11 15:23:50 +0530

    separate assign ranks function & bug fixes

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ import socket
 import threading
 import pandas as pd
 from requests import get
-from time import sleep
+from time import sleep, time
 from base64 import b64decode
 from retriever.searcher import search
 
@@ -26,13 +26,57 @@ def remove_html_tags(text):
                     re.sub(r'[\r\n\t]', '', text))))
     return text
 
-def rank_data(query, data):
+def calculate_rank(query, data):
     """ Sort data by similarity to query """
     pages = list(data.values())
     df = pd.DataFrame(pages, columns=['pages'])
     df['pages'] = df['pages'].apply(lambda x:[x])
     scores, pages = search(query, df)
     return scores, list(map(lambda x: x.tolist()[0][0], pages))
+
+def assign_ranks(final=False):
+    """ Assign ranks and save result """
+    global locked, \
+            depth, \
+            pos_q, \
+            neg_q, \
+            data
+
+    if not locked and (len(data) > 15*(depth+1) or final):
+        locked = True
+
+        scores, sorted_pages = calculate_rank(' '.join(query), data)
+
+        data = {}
+
+        try:
+            sorted_urls = list(map(lambda x: x[:x.index(' || ')], sorted_pages))
+        except Exception:
+            print(f'\r{" "*cols()}\r{red}|| ERROR{reset}')
+            locked = False
+            return 1
+
+        for url in reversed(sorted_urls):
+            # If url has children shift all children to beginning of q
+            if url in children:
+                pos_q = children[url][0] + list(set(pos_q)-set(children[url][0]))
+                neg_q = children[url][1] + list(set(neg_q)-set(children[url][1]))
+
+        clrline()
+        separator = '\n' + ' ' * 24
+        toprint = separator.join(sorted_urls[:5])
+        print(f'{yellow}\nBest result at depth {depth}: {toprint}{reset}\n')
+        depth += 1
+
+        with open('results.txt', 'a') as f:
+            for i in range(3):
+                a = sorted_pages[i].index(' || ')
+                f.write(f'{sorted_urls[i]} - {sorted_pages[i][a+4:a+56]}\n')
+            f.write('\n')
+
+        locked = False
+
+    return 0
 
 def main():
     """ Actual searcher """
@@ -47,25 +91,30 @@ def main():
         neg_q,         \
         blacklist
 
-    cols = lambda: shutil.get_terminal_size().columns
-    clrline = lambda : print('\r' + ' '*cols() + '\r', end="", flush=True)
     q_len = lambda: len(pos_q) + len(neg_q)
     print_status = lambda: print((f'\r{" "*cols()}\r{green}Depth: {depth}  Queued: {q_len()}  '
         f'Searched: {len(data)}  Current: {curr[:70]+"..." if len(curr)>75 else curr}{reset}'), end='', flush=True)
 
-    while depth < 5:
-        # if should_exit:
-        #     exit(0)
+    while True:
+        if should_exit:
+            break
 
+        start = time()
         while q_len() == 0 or locked:
-            sleep(0.1)
+            sleep(0.5)
+            if(time() - start > 10):
+                if len(data)>0:
+                    assign_ranks(final=True)
+
+                should_exit = True
+                return
 
         if len(pos_q) > 0:
             curr = pos_q.pop(0)
             timeout = 10
         else:
             curr = neg_q.pop(0)
-            timeout = 7
+            timeout = 9
 
         print_status()
 
@@ -73,9 +122,8 @@ def main():
 
         # if subdirs of site have been visited more than 20 times then dont visit site
         # again
-        # if len(list(filter(lambda x: x.startswith(curr[:delim]), visited))) < 50:
-        if curr not in visited:
-            visited.append(curr)
+        if len(list(filter(lambda x: x.startswith(curr[:delim]), visited))) < 50:
+            visited.append(curr[:delim])
         else:
             continue
 
@@ -98,20 +146,16 @@ def main():
 
         if len(tosearch) < 30 or any(list(map(lambda x: x in tosearch, blacklist))):
             continue
-        else:
-            # Doing this because after ranking pages we can grab url easily
-            data[curr] = curr + ' || ' + remove_html_tags(tosearch)
 
-        score, _ = rank_data(' '.join(query), {'1': tosearch})
+        score, _ = calculate_rank(' '.join(query), {'1': tosearch})
         # print(curr, score)
 
-        if score[0] > 0.02:
+        if score[0] > (0.02+0.01*(depth)):
+            # Doing this because after ranking pages we can grab url easily
+            data[curr] = curr + ' || ' + remove_html_tags(tosearch)
             complete_urls = set(re.findall(url_regex, tosearch))
             unique_complete = complete_urls - set(visited) - (set(pos_q).union(set(neg_q)))
 
-            # print(f"score for {curr}:", score)
-
-            # if score[0] > 0.1:
             subdirs = set(map(lambda x: curr[:delim] + x if x.startswith('/') else curr[:delim] + '/' + x, 
                 re.findall(subdir_regex, tosearch)))
             unique_subdirs = subdirs - set(visited) - (set(pos_q).union(set(neg_q)))
@@ -122,6 +166,7 @@ def main():
 
             # Positive set contains all the url that contain one or more keywords in them
             positive_set = set(filter(lambda x: any(list(keyword in x.lower() for keyword in query)), unique))
+
             # Everything else goes in negative_set
             negative_set = unique - positive_set
 
@@ -133,6 +178,7 @@ def main():
                     clrline()
                     print('\n'.join(list(negative_set)[:30]))
                 if len(positive_set) > 0:
+                    clrline()
                     print('\n'.join(positive_set))
 
                 children[curr] = [list(positive_set), list(negative_set)]
@@ -140,69 +186,17 @@ def main():
             else:
                 print(f'\r{" "*cols()}\r{red}No links found in {curr}{reset}')
 
-        if not locked and len(data) > 20*(depth+1):
-            locked = True
-
-            date_copy = data.copy()
-            scores, sorted_pages = rank_data(' '.join(query), date_copy)
-
-            # if depth == 1:
-            #     prevscores = scores
-            # else:
-            #     if sum(scores[0]) < sum(prevscores[:5]):
-            #         clrline()
-            #         print(f'{red}Better results in previous depth, exiting{reset}')
-            #         should_exit = True
-            #         exit(0)
-
-            data = {}
-
-            try:
-                sorted_urls = list(map(lambda x: x[:x.index(' || ')], sorted_pages))
-            except Exception:
-                print(f'\r{" "*cols()}\r{red}|| ERROR{reset}')
-                print(sorted_urls)
-                locked = False
+            if assign_ranks() == 1:
                 continue
 
-            separator = '\n' + ' ' * 24
-            toprint = separator.join(sorted_urls[:5])
-
-            clrline()
-            depth += 1
-            print(f'{yellow}\nBest result at depth {depth}: {toprint}{reset}\n')
-
-            with open('results.txt', 'a') as f:
-                a = sorted_pages[0].index(' || ')
-                b = sorted_pages[1].index(' || ')
-                f.write((f'{depth}. {sorted_urls[0]} - {sorted_pages[0][a+4:a+56]}\n'
-                        f'{depth}. {sorted_urls[1]} - {sorted_pages[1][b+4:b+56]}\n\n'))
-
-            for url in reversed(sorted_urls[:3]):
-                # If url has children shift all children to beginning of q
-                if url in children:
-                    pos_q = children[url][0] + pos_q
-                    neg_q = children[url][1][:20] + neg_q
-                else:
-                    print(f'{url} has no children')
-
-            locked = False
 
 if __name__ == '__main__':
+    cols = lambda: shutil.get_terminal_size().columns
+    clrline = lambda : print('\r' + ' '*cols() + '\r', end="", flush=True)
+
     query = list(map(lambda x: x.lower(), [
             'flipkart',
-            'data',
-            'leaked',
-            'cybercrime',
-            'hacking',
         ]))
-
-    # query = list(map(lambda x: x.lower(), [
-    #         "cats",
-    #         "dogs",
-    #         "animals"
-    #     ]))
-
 
     banned_types = "|".join([
             'png',
@@ -219,8 +213,7 @@ if __name__ == '__main__':
             'woff',
             'eot',
             'ttf',
-            'zip',
-            'xmlrpc.php'
+            'zip'
         ])
 
     url_regex = rf'((?:https?:\/\/)\w+.onion\/?(?!\S+?(?:{banned_types}))[\w\/\-%\.\?]*\/?)'
@@ -257,11 +250,17 @@ if __name__ == '__main__':
     # Clear results file
     open('results.txt', 'w').close()
 
+    # Load blacklist
     blacklist = []
     with open('blacklist.txt') as f:
         for line in f:
             blacklist.append(b64decode(line).decode())
 
+    if any(list(word in query for word in blacklist)):
+        print(f'{yellow}Blacklist disabled because query contains blacklisted token{reset}')
+        blacklist = []
+
+    # Use search engine results for query as starting urls for crawler
     print(f'{yellow}Grabbing results from phobos{reset}')
     neg_q = []
     for i in range (1,4):
@@ -286,6 +285,6 @@ if __name__ == '__main__':
     pos_q = []
 
     threads=[]
-    for _ in range(7):
+    for _ in range(6):
         threads.append(threading.Thread(target=main))
         threads[-1].start()
